b1955574adcf2f645e9e29439ff808032f8a04ed
shenixo <abdulrazaqolaseni@gmail.com>
2026-02-06 09:17:17 +0100

    removed the base url from .env

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL: "https://ulooks-api.vercel.app/api/v1/",
+  // baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
